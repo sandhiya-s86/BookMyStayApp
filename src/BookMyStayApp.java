b217b013104c9f1dd3b3a32e5416cc86a09a2639
sandhiya-s86 <sandhiya.official86@gmail.com>
2026-04-01 09:27:38 +0530

    UC1

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("      Welcome to Book My Stay App      ");
+        System.out.println("   Hotel Booking Management System     ");
+        System.out.println("             Version 1.0               ");
+        System.out.println("=======================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay.");
+    }
+}
